3ae9ed8f123725b5094faa933bd5f26e4432e0d8
Nis5680 <nishitshah5680@gmail.com>
2025-12-09 15:45:42 +0530

    1st_change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('condition-app');
+
+  display=true;
+  x=10;
+  hide() {
+    this.display = false;
+  }
+  show() {
+    this.display = true;
+  }
 }
